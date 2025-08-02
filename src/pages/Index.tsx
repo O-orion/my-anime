8573c6f6fab4a93e11b2b991e1908e5fb2fbf82b
Lucas Viana Torres <lucas.lvtorres@gmail.com>
2025-08-02 13:04:03 -0300

    Add initial animes data and implement posts state management in Index component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,19 @@
+import posts from "@/data/animes.json";
+import { useEffect, useState } from "react";
 
 const Index = () => {
 
+    const [posts, setPosts] = useState([]);
+    const [activeTab, setActiveTab] = useState<"foryou" | "following">("foryou")
+
+    useEffect(() => {
+        setPosts(posts)
+    }, posts)
+
     return (
-        
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+            
+        </div>
     )
 
 }
